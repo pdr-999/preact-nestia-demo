d40376475c81ccf2847a0d9388c067cd6fe7c5e3
Alva A <rpondering@gmail.com>
2023-08-20 19:09:18 +0700

    refactor: prettier all files

--- pages/404.page.tsx
+++ pages/404.page.tsx
@@ -1,8 +1,8 @@
 export function Page() {
-	return (
-		<section>
-			<h1>404: Not Found</h1>
-			<p>It's gone :(</p>
-		</section>
-	);
+  return (
+    <section>
+      <h1>404: Not Found</h1>
+      <p>It's gone :(</p>
+    </section>
+  );
 }
